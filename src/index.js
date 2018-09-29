c515deced19ba6ad22d341c81c0c18ff20a7f940
Bartłomiej Rasztabiga <bartlomiej.rasztabiga.official@gmail.com>
2018-09-29 17:42:51 +0200

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,20 @@
+import React from "react"
+import ReactDOM from "react-dom"
+import { BrowserRouter as Router, Route } from "react-router-dom"
+import { Provider } from "react-redux"
+
+import store from "./store"
+import Dashboard from "./containers/Dashboard"
+
+import "bootstrap/dist/css/bootstrap.min.css"
+import "./index.css"
+//import "react-tippy/dist/tippy.css";
+
+ReactDOM.render(
+  <Provider store={store}>
+    <Router>
+      <Route path="/" component={Dashboard} />
+    </Router>
+  </Provider>,
+  document.getElementById("root")
+)
