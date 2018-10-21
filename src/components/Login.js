05c299f8899fba97f651493791af898966db4604
Bartłomiej Rasztabiga <bartlomiej.rasztabiga.official@gmail.com>
2018-10-21 21:55:30 +0200

    WIP: network error on submit

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,44 +1,75 @@
-import React from "react"
+import React, { Component } from "react"
+import { connect } from "react-redux"
+
+import { login } from "../actions"
+
 import "./Login.css"
 
-export default function Login(props) {
-  return (
-    <div class="container">
-      <div class="login-form">
-        <div class="main-div">
-          <div class="panel">
-            <h2>RedAlert Logowanie</h2>
-            <p>Wprowadź login i hasło do konta urzędnika</p>
-          </div>
-          <form id="Login">
-            <div class="form-group">
-              <input
-                type="email"
-                class="form-control"
-                id="inputEmail"
-                placeholder="Adres e-mail"
-              />
-            </div>
+export class Login extends Component {
+  state = {
+    email: "",
+    password: ""
+  }
 
-            <div class="form-group">
-              <input
-                type="password"
-                class="form-control"
-                id="inputPassword"
-                placeholder="Hasło"
-              />
+  submitLogin = e => {
+    e.preventDefault()
+    this.props.login(this.state.email, this.state.password)
+  }
+
+  render() {
+    return (
+      <div class="container">
+        <div class="login-form">
+          <div class="main-div">
+            <div class="panel">
+              <h2>RedAlert Logowanie</h2>
+              <p>Wprowadź login i hasło do konta urzędnika</p>
             </div>
-            <div class="forgot">
-              <a href="reset.html">
-                Zapomniałeś hasła? <small>nie nasz problem</small>
-              </a>
-            </div>
-            <button type="submit" class="btn btn-primary">
-              Zaloguj się
-            </button>
-          </form>
+            <form id="Login" onSubmit={this.submitLogin.bind(this)}>
+              <div class="form-group">
+                <input
+                  type="email"
+                  class="form-control"
+                  id="inputEmail"
+                  placeholder="Adres e-mail"
+                  value={this.state.email}
+                  onChange={e => this.setState({ email: e.target.value })}
+                />
+              </div>
+
+              <div class="form-group">
+                <input
+                  type="password"
+                  class="form-control"
+                  id="inputPassword"
+                  placeholder="Hasło"
+                  value={this.state.password}
+                  onChange={e => this.setState({ password: e.target.value })}
+                />
+              </div>
+              <div class="forgot">
+                <a href="reset.html">
+                  Zapomniałeś hasła? <small>nie nasz problem</small>
+                </a>
+              </div>
+              <button type="submit" class="btn btn-primary">
+                Zaloguj się
+              </button>
+            </form>
+          </div>
         </div>
       </div>
-    </div>
-  )
+    )
+  }
 }
+
+const mapDispatchToProps = dispatch => {
+  return {
+    login: (email, password) => dispatch(login(email, password))
+  }
+}
+
+export default connect(
+  null,
+  mapDispatchToProps
+)(Login)
--- src/services/user.service.js
+++ src/services/user.service.js
@@ -1,12 +1,15 @@
 import axios from "axios"
 import qs from "qs"
+import { ACCESS_TOKEN_ITEM_NAME } from "../constants"
 
-export const login = async (email, password) => {
+const login = async (email, password) => {
   const apiRoot = process.env.REACT_APP_SERVER_URL
   const data = { access_token: process.env.REACT_APP_API_MASTER_KEY }
 
+  let url = `${apiRoot}/auth/`
+
   let response = await axios.post({
-    url: `${apiRoot}/auth/`,
+    url: url,
     auth: {
       email: email,
       password: password
@@ -20,9 +23,23 @@ export const login = async (email, password) => {
   let user = response.body.user
   let token = response.body.token
 
+  let result
+
   if (user && token) {
-    localStorage.setItem("access_token", token)
+    localStorage.setItem(ACCESS_TOKEN_ITEM_NAME, token)
+    result.user = user
+  } else {
+    result.error = response.body
   }
 
-  return user
+  return result
+}
+
+const logout = () => {
+  localStorage.removeItem(ACCESS_TOKEN_ITEM_NAME)
+}
+
+export const userService = {
+  login,
+  logout
 }
